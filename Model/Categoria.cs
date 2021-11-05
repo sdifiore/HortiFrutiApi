e4f322d1844e70a7e7e8783d0b8b9f6a32252ba2
Sergio Di Fiore <difiores@outlook.com>
2021-11-05 20:39:36 -0300

    Add project files.

---
+++ Model/Categoria.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+
+namespace HortiFrutiApi.Model
+{
+    [Table("Categorias")]
+    public class Categoria
+    {
+        public int Id { get; set; }
+
+        [Column(TypeName = "VARCHAR(4)")]
+        public string Apelido { get; set; }
+
+        [MaxLength(60)]
+        public string Descricao { get; set; }
+    }
+}
---
+++ Model/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace HortiFrutiApi.Model
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+    }
+}
